8af11d315e81e189e435f22e7645d914aa69e5a3
bird-grid2 <bird2.one.regend@gmail.com>
2020-07-22 00:33:49 +0900

    create_graph_function

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,6 +1,16 @@
 class GraphsController < ApplicationController
 
   def index
+    @user = User.find(params[:id])
+    require 'pycall/import'
+    include PyCall::Import
+    pyimport 'matplotlib.pyplot', as: 'plt'
+    plt.title('height graph')
+    plt.xlabel('measurement date')
+    plt.ylabel('height')
+    plt.plot([70,60,50,90])
+    @graph = plt.show()
+
   end
 
 end
