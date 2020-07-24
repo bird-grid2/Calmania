eef625bf405c10028aa9740461b89f87700cab5c
bird-grid2 <bird2.one.regend@gmail.com>
2020-07-24 23:29:13 +0900

    updated_graphs_controller

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,10 +1,25 @@
 class GraphsController < ApplicationController
-
+  
+  require 'pycall/import'
+  include PyCall::Import
+  require 'numpy'
+  np = Numpy
+  
   def index
-    @user = User.find(params[:id])
-    require 'pycall/import'
-    include PyCall::Import
-    pyimport 'matplotlib.pyplot', as: 'plt'
+    require 'pg'
+    
+    # データベース接続する
+    connection = PG::connect(host: "localhost", user: "yondo", dbname: "Calmania_development", port: "5432")
+    begin
+      result = connection.exec("SELECT * FROM User")
+    ensure
+      # データベースへのコネクションを切断する
+      connection.finish
+    end
+    @user = result.find(params[:id])
+    
+    
+
     plt.title('height graph')
     plt.xlabel('measurement date')
     plt.ylabel('height')
