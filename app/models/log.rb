88600051f0afe5fa92cdc513b888c149950f9064
bird-grid2 <bird2.one.regend@gmail.com>
2020-08-04 23:55:28 +0900

    Update log.rb

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,6 +3,6 @@ class Log < ApplicationRecord
   belongs_to :user
   belongs_to :management
   has_many :log_menus
-  has_many :menus, thorough: :log_menus
+  has_many :menus, through: :log_menus
 
 end
