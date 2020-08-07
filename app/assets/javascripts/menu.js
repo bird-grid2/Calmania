835758c3c4eeb856487b166fc1adb0443b495347
bird-grid2 <bird2.one.regend@gmail.com>
2020-08-08 01:52:06 +0900

    updated_menu.js

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -6,14 +6,17 @@ $(document).on('turbolinks:load', function(){
 
     function buildHTML(count) {
       var html =  `<div class='input_form__column__protain'> Protain
-                    <p>${count.protain_rate * mass}</p>
+                    <p>${Math.ceil(count.protain_rate * mass * 4)/10} [kCal]</p>
                    </div>
                    <div class='input_form__column__fat'> Fat
-                    <p>${count.fat_rate * mass}</p>
+                    <p>${Math.ceil(count.fat_rate * mass * 9)/10} [kCal]</p>
                    </div>
                    <div class='input_form__column__carbohydrate'> Carbohydrate
-                    <p>${count.carbohydrate_rate * mass}</p>
-                   </div>`
+                    <p>${Math.ceil(count.carbohydrate_rate * mass * 4)/10} [kCal]</p>
+                   </div>
+                   <div class='input_form__column__total_mass'> Total
+                   <p>${Math.ceil((count.protain_rate * mass * 4) + (count.fat_rate * mass * 9) + (count.carbohydrate_rate * mass * 4))/10} [kCal]</p>
+                  </div>`
       return html;
     };
     
