02fea42cd45c20fa6cf1f70a044ca88236295bf6
bird-grid2 <bird2.one.regend@gmail.com>
2020-08-09 02:25:41 +0900

    Update menu.js

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -5,23 +5,33 @@ $(document).on('turbolinks:load', function(){
     var mass = $(this).val();
     var id = $(this).parent().parent().children().val();
     var target = gon.food[(id - 1)];
+    var protain_cal = Math.ceil(target.protain_rate * mass * 4)/10;
+    var fat_cal = Math.ceil(target.fat_rate * mass * 9)/10;
+    var carbohydrate_cal = Math.ceil(target.carbohydrate_rate * mass * 4)/10
     var protain = [];
     var fat = [];
     var carbohydrate = [];
+    var sum  = (arr)=>{
+      var sum = 0;
+      arr.forEach((elm)=>{
+          sum += elm;
+      });
+      return sum;
+    };
 
     //PFCのカロリーを計算して、HTMLに追加する関数の定義
     function buildHTML(count) {
       var html =  `<div class='input_form__column__protain'> Protain
-                    <p>${Math.ceil(count.protain_rate * mass * 4)/10} [kCal]</p>
+                    <p>${protain_cal} [kCal]</p>
                    </div>
                    <div class='input_form__column__fat'> Fat
-                    <p>${Math.ceil(count.fat_rate * mass * 9)/10} [kCal]</p>
+                    <p>${fat_cal} [kCal]</p>
                    </div>
                    <div class='input_form__column__carbohydrate'> Carbohydrate
-                    <p>${Math.ceil(count.carbohydrate_rate * mass * 4)/10} [kCal]</p>
+                    <p>${carbohydrate_cal} [kCal]</p>
                    </div>
                    <div class='input_form__column__total_mass'> Total
-                   <p>${Math.ceil((count.protain_rate * mass * 4) + (count.fat_rate * mass * 9) + (count.carbohydrate_rate * mass * 4))/10} [kCal]</p>
+                   <p>${protain_cal + fat_cal + carbohydrate_cal} [kCal]</p>
                   </div>`
       return html;
     };
@@ -31,24 +41,27 @@ $(document).on('turbolinks:load', function(){
     $(this).parent().parent().parent().children().last().append(buildHTML(target));
 
     //それぞれの成分を配列に挿入
-    protain.append(Math.ceil(target.protain_rate * mass * 4)/10);
-    fat.append(Math.ceil(target.fat_rate * mass * 9)/10);
-    carbohydrate.append(Math.ceil(target.carbohydrate_rate * mass * 4)/10);
+    protain.push(protain_cal);
+    fat.push(fat_cal);
+    carbohydrate.push(carbohydrate_cal);
+
+    console.log(protain)
 
     //計算結果のHTML定義
     function calcHTML(target) {
       var html = `<p> ${sum(target)} [kCal]</p>`
+      return html;
     };
 
     //計算結果の挿入
     $('.calculate_box--protain').children().remove();
-    $('.calculate_box--protain').children().append(calcHTML(protain));
+    $('.calculate_box--protain').append(calcHTML(protain));
 
     $('.calculate_box--fat').children().remove();
-    $('.calculate_box--fat').children().append(calcHTML(fat));
+    $('.calculate_box--fat').append(calcHTML(fat));
 
     $('.calculate_box--carbohydrate').children().remove();
-    $('.calculate_box--carbohydrate').children().append(calcHTML(carbohydrate));
+    $('.calculate_box--carbohydrate').append(calcHTML(carbohydrate));
 
   });
 });
