20b535e32a3a5a410a570d6145c4ed65077fdb1a
bird-grid2 <bird2.one.regend@gmail.com>
2020-10-03 23:04:29 +0900

    updating_food.rb

--- spec/models/food_rspec.rb
+++ spec/models/food_rspec.rb
@@ -1,18 +1,33 @@
 require 'rails-helper'
 
-describe Food do
-  context 'validation' do
-    describe '#create' do
+describe Food do  
+  describe '#create' do
+    context 'validation' do
       it 'food名が空なら登録できない' do 
         foods = Food.new(food: nil, protain_rate: '0.12345', fat_rate: '0.67890', carbohydrate_rate: '0.12345')
         foods.valid?
-        expect(foods.errors[:food]).to incluude("can't be blank")
+        expect(foods.errors[:food]).to include("can't be blank")
       end
       it 'protain_rateが空なら登録できない' do
         foods = Food.new(food: 'サンプルA', protain_rate: nil, fat_rate: '0.67890', carbohydrate_rate: '0.12345')
         foods.valid?
-        expect(foods.errors[:protain_rate]).to incluude("can't be blank")
+        expect(foods.errors[:protain_rate]).to include("can't be blank")
       end
+      it 'fat_rateが空なら登録できない' do 
+        foods = Food.new(food: 'サンプルA', protain_rate: '0.12345', fat_rate: nil, carbohydrate_rate: '0.12345')
+        foods.valid?
+        expect(foods.errors[:fat_rate]).to include("can't be blank")
+      end
+      it 'carbohydrate_rateが空なら登録できない' do 
+        foods = Food.new(food: 'サンプルA', protain_rate: '0.12345', fat_rate: '0.67890', carbohydrate_rate: nil)
+        foods.valid?
+        expect(foods.errors[:carbohydrate_rate]).to include("can't be blank")
+      end
+    end
+    context 'submit' do
+      it '全てが入力されていると、登録できる' do
+          
+    end
     end
   end
 end
