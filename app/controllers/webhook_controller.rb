da038c2db5c236a134a68d37ca427c727fc31620
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-13 06:37:00 +0900

    modified_rubocop_ indicating_ thing

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,22 +1,20 @@
 class WebhookController < ApplicationController
   require 'line/bot'  # gem 'line-bot-api'
 
-  protect_from_forgery except: [:callback] # CSRF対策無効化
+  protect_from_forgery except: [:callback] # CSRF protection
 
   def client
-    @client ||= Line::Bot::Client.new { |config|
+    @client ||= Line::Bot::Client.new  |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
+    
   end
 
   def callback
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
-    end
+      halt 400, { 'Content-Type' => 'text/plain' }, 'Bad Request' unless client.validate_signature(body, signature)
 
     events = client.parse_events_from(body)
 
--- spec/requests/webhook_request_spec.rb
+++ spec/requests/webhook_request_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Webhooks", type: :request do
-
 end
