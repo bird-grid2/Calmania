414ba26b5a7207455ce20dd0c3efb61c75022492
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-13 07:00:57 +0900

    Update webhook_controller.rb

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -4,7 +4,9 @@ class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF protection
 
   def client
-    @client ||= Line::Bot::Client.new { |config| config.channel_secret = ENV["LINE_CHANNEL_SECRET"] config.channel_token = ENV["LINE_CHANNEL_TOKEN"] }
+    @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"] }
   end
 
   def callback
