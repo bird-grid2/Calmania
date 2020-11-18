ef39a3824f8a6816b86e3db4cec0e343f0c5534a
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-19 07:02:12 +0900

    creating_clock.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,9 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
   validates :height, :ideal_protain_rate, :ideal_fat_rate, :ideal_carbohydrate_rate, :target_cal, numericality: { allow_nil: true }
+
+  def if?(time)
+    time.day == day && time.month == month
+  end
   
 end
--- clock.rb
+++ clock.rb
@@ -3,25 +3,40 @@ require 'active_support/time'
 require './config/boot'
 require './config/environment'
 
-
 module Clockwork
+  Clockwork.manager = DatabaseEvents::Manager.new
+
+  container = 0
+
+  sync_database_events model: Users, every: 1.hour do |model_instance|
+    container = model_instance.period
+  end
+
   handler do |job|
     case job
     when '1day.job'
-      # 1day
+      broadcast()
     when '2days.job'
-      # 2days
+      broadcast()
     when '3days.job'
-      # 3days
+      broadcast()
+    when '4days.job'
+      broadcast()
     when '1week.job'
-      # 1week
+      broadcast()
     end
   end
 
-  every(1.day, '1day.job')
-  every(2.day, '2days.job')
-  every(3.day, '3days.job')
-  every(7.day, '1week.job')
+  if container == '毎日'
+    every(1.day, '1day.job')
+  elsif container == '1日毎'
+    every(2.day, '2days.job')
+  elsif container == '2日毎'
+    every(3.day, '3days.job')
+  elsif container == '3日毎'
+    every(4.day, '1week.job')
+  elsif container == '1週間毎'
+    every(7.day, '1week.job')
 
   configure do |config|
     config[:sleep_timeout] = 5
@@ -30,5 +45,4 @@ module Clockwork
     config[:max_threads] = 15
     config[:thread] = true
   end
-
 end
