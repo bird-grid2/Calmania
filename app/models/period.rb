da47e817f603074f66aea712e3433b964e1dbe49
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-18 20:47:25 +0900

    Update period.rb

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,10 +1,10 @@
 class Period < ActiveHash::Base
   self.data = [
-    {id: 0, name: '選択して下さい'}, 
-    {id: 1, name: '毎日'}, 
-    {id: 2, name: '1日毎'}, 
-    {id: 3, name: '2日毎'}, 
-    {id: 4, name: '3日毎'}, 
-    {id: 5, name: '1週間毎'} 
+    { id: 0, name: '選択して下さい' },
+    { id: 1, name: '毎日' },
+    { id: 2, name: '1日毎' },
+    { id: 3, name: '2日毎' },
+    { id: 4, name: '3日毎' },
+    { id: 5, name: '1週間毎' }
   ]
 end
