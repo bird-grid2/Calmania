c8d100547f415314138d02287765eeb0b6786df0
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-19 22:14:27 +0900

    modifed_rubocup_indicating_thing

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -37,8 +37,6 @@ class WebhookController < ApplicationController
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      halt 400, { 'Content-Type' => 'text/plain' }, 'Bad Request'
-    end
+    halt 400, { 'Content-Type' => 'text/plain' }, 'Bad Request' unless client.validate_signature(body, signature)
   end
 end
--- clock.rb
+++ clock.rb
@@ -7,11 +7,11 @@ module Clockwork
   Clockwork.manager = DatabaseEvents::Manager.new
 
   container = 0
-  time = 0
+  timer = 0
 
   sync_database_events model: Users, every: 1.hour do |model_instance|
     container = model_instance.period
-    time = model_instance.send_time
+    timer = model_instance.send_time
   end
 
   handler do |job|
@@ -31,15 +31,15 @@ module Clockwork
 
   case container
   when '毎日'
-    every(1.day, '1day.job', at: time)
+    every(1.day, '1day.job', at: timer)
   when '1日毎'
-    every(2.day, '2days.job', at: time)
+    every(2.day, '2days.job', at: timer)
   when '2日毎'
-    every(3.day, '3days.job', at: time)
+    every(3.day, '3days.job', at: timer)
   when '3日毎'
-    every(4.day, '4days.job', at: time)
+    every(4.day, '4days.job', at: timer)
   when '1週間毎'
-    every(7.day, '1week.job', at: time)
+    every(7.day, '1week.job', at: timer)
   end
 
   configure do |config|
