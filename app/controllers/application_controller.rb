eba3b161291cd2aa2d9e8f07ea4b67f5dc215879
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-21 22:13:29 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,19 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource) 
-    managements_path(resource)
+    if current_user
+      managements_path(resource)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    if current_user
+      user_path(resource)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   protected
