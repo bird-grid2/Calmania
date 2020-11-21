e936f98eb94528c393ccba55af986c2637ff0c08
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-21 20:55:32 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    add_list = [ :nickname, :email, :height, :ideal_protain_rate, :ideal_fat_rate, :ideal_carbohydrate_rate, :password, :password_confirmation, :period_id, :send_time]
-    devise_parameter_sanitizer.permit(:sign_up, keys: add_list)
-    devise_parameter_sanitizer.permit(:account_update, keys: add_list)
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname, :email, :password])
+    add_list = [ :nickname, :email, :height, :ideal_protain_rate, :ideal_fat_rate, :ideal_carbohydrate_rate, :password, :password_confirmation, :period_id, :send_time ]
+    devise_parameter_sanitizer.permit :sign_up, keys: add_list
+    devise_parameter_sanitizer.permit :account_update, keys: add_list
+    devise_parameter_sanitizer.permit :sign_in, keys: [:nickname, :email, :password]
   end
   
 end
