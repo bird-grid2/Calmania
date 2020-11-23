7f90b92be08d86304a8950be8400663942fa5721
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-23 22:28:43 +0900

    modified_controller_thing

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,24 +1,22 @@
 # frozen_string_literal: true
 
-class Users < Devise::SessionsController
-  class SessionsController
-    before_action :authenticate_user!, except: [:new, :create]
-    before_action :configure_sign_in_params, only: [:create]
+class Users::SessionsController < Devise::SessionsController
+  before_action :authenticate_user!, except: [:new, :create]
+  before_action :configure_sign_in_params, only: [:create]
 
-    protected
+  protected
 
-    # If you have extra params to permit, append them to the sanitizer.
-    def configure_sign_in_params
-      add_list = [:nickname, :email, :height, :ideal_protain_rate, :ideal_fat_rate, :ideal_carbohydrate_rate, :password, :password_confirmation, :period_id, :send_time, :target_cal]
-      devise_parameter_sanitizer.permit(:sign_in, keys: add_list)
-    end
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    add_list = [:nickname, :email, :height, :ideal_protain_rate, :ideal_fat_rate, :ideal_carbohydrate_rate, :password, :password_confirmation, :period_id, :send_time, :target_cal]
+    devise_parameter_sanitizer.permit(:sign_in, keys: add_list)
+  end
 
-    def after_sign_up_path_for(*)
-      managements_path
-    end
+  def after_sign_up_path_for(*)
+    managements_path
+  end
 
-    def after_update_path_for(*)
-      managements_path
-    end
+  def after_update_path_for(*)
+    managements_path
   end
 end
