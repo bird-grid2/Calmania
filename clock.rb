af458eec0b941fbb258e5c525978a5c0d6d5ffbb
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-26 21:43:29 +0900

    modified_webhook_controller

--- clock.rb
+++ clock.rb
@@ -1,38 +1,38 @@
 module Clockwork
   def send(target)
     url = 'https://api.line.me/v2/bot/message/broadcast'
-  
+
     access_token = ENV["LINE_ACCESS_TOKEN"]
-  
+
     text_data = {
       messages: [{
         type: 'text',
         text: '時間になりました。</br>定期入力の時間です。'
       }]
     }
-  
+
     headers = {
       "Content-Type": "application/json",
       "Authorization": "Bearer #{access_token}",
     }
-  
+
     option = {
       method: 'post',
       headers: headers,
       payload: JSON.stringify(text_data)
     }
-  
+
     broadcast = UrlFetchApp.fetch(url, option)
     Clockwork.manager = DatabaseEvents::Manager.new
-  
+
     container = 0
     timer = 0
-  
+
     sync_database_events model: Users, every: 1.hour do |model_instance|
       container = model_instance.find_by(id: target).period
       timer = model_instance.find_by(id: target).send_time
     end
-  
+
     handler do |job|
       case job
       when '1day.job'
@@ -47,7 +47,7 @@ module Clockwork
         broadcast
       end
     end
-  
+
     case container
     when '毎日'
       every(1.day, '1day.job', at: timer)
@@ -60,7 +60,7 @@ module Clockwork
     when '1週間毎'
       every(7.day, '1week.job', at: timer)
     end
-  
+
     configure do |config|
       config[:sleep_timeout] = 5
       config[:logger] = Logger.new(log_file_path)
