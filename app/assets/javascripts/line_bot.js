09856252d26a81a3b461ef9d05d2f35ba39cfc56
bird-grid2 <bird2.one.regend@gmail.com>
2020-11-28 12:58:46 +0900

    Update line_bot.js

--- app/assets/javascripts/line_bot.js
+++ app/assets/javascripts/line_bot.js
@@ -1,11 +1,12 @@
 
 $(document).on('turbolinks:load', function(){
-  if(document.URL.match(/users*edit/)){
+  if(document.URL.match(/users\/\edit/)){
     $('#edit_submit').on('click', ()=>{
       var url = "https://calmania.work/broadcast"
       var request = createXMLHttpRequest();
       request.open("POST", url, true);
       request.send("");
+      console.log("ok")
     });
   };
 });
