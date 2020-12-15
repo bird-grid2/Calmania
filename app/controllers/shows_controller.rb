735f5c0446d9d0163b2a7cafa3b3caf4c677cbc3
bird-grid2 <bird2.one.regend@gmail.com>
2020-12-15 20:32:20 +0900

    modified_webhook_controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,3 +1,35 @@
 class ShowsController < ApplicationController
+  require 'line/bot'  # gem 'line-bot-api'
+  before_action :client, only: [:broadcast]
+
+  protect_from_forgery except: [:broadcast] # CSRF protection
+
   def index; end
+
+  def broadcast
+    messages = {
+      type: 'text',
+      text: '時間になりました。</br>定期入力の時間です。'
+    }
+    client.broadcast(messages)
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      congig.channel_id = ENV["LINE_CHANNEL_ID"]
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_ACCESS_TOKEN"]
+    end
+  end
+
+  def validates_signature
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    halt 400, { 'Content-Type' => 'text/plain' }, 'Bad Request' unless client.validate_signature(body, signature)
+  end
+end
+
 end
--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -25,14 +25,6 @@ class WebhookController < ApplicationController
     "OK"
   end
 
-  def broadcast
-    messages = {
-      type: 'text',
-      text: '時間になりました。</br>定期入力の時間です。'
-    }
-    client.broadcast(messages)
-  end
-
   private
 
   def client
