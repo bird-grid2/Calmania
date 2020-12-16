690d21bb7387be3341edb9407762137f371c6f70
bird-grid2 <bird2.one.regend@gmail.com>
2020-12-17 06:41:56 +0900

    Update webhook_controller.rb

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -10,16 +10,15 @@ class WebhookController < ApplicationController
     end
   end
 
-  post '/callback' do
+  def callback
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      halt 400, { 'Content-Type' => 'text/plain' }, 'Bad Request'
+    unless client.validate_signature(body, signature) 
+      error 400 do 'Bad Request' end 
     end
 
     events = client.parse_events_from(body)
-
     events.each do |event|
       case event
       when Line::Bot::Event::Message
@@ -37,7 +36,6 @@ class WebhookController < ApplicationController
         end
       end
     end
-
     "OK"
   end
 
