c262b64dd642b39c797c06aa1d54d99111e7a8da
bird-grid2 <bird2.one.regend@gmail.com>
2020-12-17 07:02:01 +0900

    Update webhook_controller.rb

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -14,8 +14,8 @@ class WebhookController < ApplicationController
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature) 
-      error 400 do 'Bad Request' end 
+    unless client.validate_signature(body, signature)
+      error 400 { 'Bad Request' }
     end
 
     events = client.parse_events_from(body)
