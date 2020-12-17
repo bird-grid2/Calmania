ddced5f4744c5107a7e68302cabadd88db4dd0d0
bird-grid2 <bird2.one.regend@gmail.com>
2020-12-17 22:59:17 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,9 @@ class ApplicationController < ActionController::Base
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      error 400 { 'Bad Request' }
+      error 400 do 
+        'Bad Request'
+      end
     end
   end
 
