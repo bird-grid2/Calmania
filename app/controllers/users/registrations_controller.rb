0ecd02623056c8bbe24254149914f412f8c67bea
bird-grid2 <bird2.one.regend@gmail.com>
2021-01-02 18:47:53 +0900

    modifing_rubocup_indicating_thing

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,7 +15,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    add_list = [:nickname, :email, :height, :ideal_protain_rate, :ideal_fat_rate, :ideal_carbohydrate_rate, :target_cal, :password, :password_confirmation, clock_work_event_attributes: [ :period_id, :send_time]]
+    add_list = [:nickname, :email, :height, :ideal_protain_rate, :ideal_fat_rate, :ideal_carbohydrate_rate, :target_cal, :password, :password_confirmation, { clock_work_event_attributes: [ :period_id, :send_time ] } ]
     devise_parameter_sanitizer.permit(:account_update, keys: add_list)
   end
 
--- spec/factories/clock_work_events.rb
+++ spec/factories/clock_work_events.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :clock_work_event do
-    
+    # Do Something
   end
 end
