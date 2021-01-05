9ce9e6a548bd1015f86af3e39762aceb0660ba3d
bird-grid2 <bird2.one.regend@gmail.com>
2021-01-06 07:10:02 +0900

    Update clock.rb

--- clock.rb
+++ clock.rb
@@ -34,7 +34,7 @@ module Clockwork
     end
   end
 
-  sync_database_events model: ClockWorkEvent, every: 1.hour, at: "**:59" do |model_instance|
+  sync_database_events model: ClockWorkEvent, every: 1.hour do |model_instance|
     list = [model_instance.send_time, model_instance.period_id, model_instance.user_id]
     timer = list[0].strftime("%H:%M")
     container = list[1]
