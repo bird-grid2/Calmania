2cc475f0e794f1dedf31f2446a847b2a3a28b357
bird-grid2 <bird2.one.regend@gmail.com>
2021-01-25 05:51:54 +0900

    modified_broadcast.job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,3 +1,4 @@
 class ApplicationJob < ActiveJob::Base
+  include Sidekiq::Worker
   include Sidekiq::Status::Worker
 end
--- app/jobs/broadcast_job.rb
+++ app/jobs/broadcast_job.rb
@@ -2,7 +2,7 @@ class BroadcastJob < ApplicationJob
   queue_as :broadcast
 
   def expiration
-    @expiration ||= 60 * 3 # 3 minutes
+    @expiration ||= 60 * 60 * 24 # 1 day
   end
 
   def perform(elem, num)
