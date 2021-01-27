cffab85df591e0e48b879c6eb55a5b6a87ea7b1e
bird-grid2 <bird2.one.regend@gmail.com>
2021-01-28 05:41:10 +0900

    Update broadcast_worker.rb

--- app/workers/broadcast_worker.rb
+++ app/workers/broadcast_worker.rb
@@ -10,17 +10,6 @@ class BroadcastWorker
 
   def perform(elem)
     @clock = ClockWorkEvent.find_by(user_id: elem)
-    case @clock.period_id
-    when 1
-      every(1.day, '1.day.job', at: @clock.send_time.strftime("%H:%M"))
-    when 2
-      every(2.days, '2.days.job', at: @clock.send_time.strftime("%H:%M"))
-    when 3
-      every(3.days, '3.days.job', at: @clock.send_time.strftime("%H:%M"))
-    when 4
-      every(4.days, '4.days.job', at: @clock.send_time.strftime("%H:%M"))
-    when 5
-      every(7.days, '1.week.job', at: @clock.send_time.strftime("%H:%M"))
-    end
+    p "hello world"
   end
 end
