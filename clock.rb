e2757e475152c24f29a215094778cee3fa02e8de
bird-grid2 <bird2.one.regend@gmail.com>
2021-01-30 21:23:42 +0900

    modified_clock.rb

--- clock.rb
+++ clock.rb
@@ -9,25 +9,6 @@ require File.expand_path('./config/environment', __dir__)
 module Clockwork
   Clockwork.manager = DatabaseEvents::Manager.new
 
-  handler do |job|
-    case job
-    when '1.day.job' || '2.days.job' || '3.days.job' || '4.days.job' || '1.week.job'
-      uri = URI.parse("https://calmania.work/send")
-      http = Net::HTTP.new(uri.host, uri.port)
-
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      message = 'test'
-
-      http.start do
-        req = Net::HTTP::Post.new(uri.path)
-        req.set_form_data({ body: message })
-        http.request(req)
-      end
-    end
-  end
-
   sync_database_events model: ClockWorkEvent, every: 4.hours do |model_instance|
     id = model_instance.user_id   
     BroadcastWorker.perform_async(id)
