f59b4f85b85b046e7e9e1b294877cbb64543c5c7
bird-grid2 <bird2.one.regend@gmail.com>
2021-02-03 06:59:40 +0900

    Update broadcast_worker.rb

--- app/workers/broadcast_worker.rb
+++ app/workers/broadcast_worker.rb
@@ -3,7 +3,7 @@ class BroadcastWorker
   include Sidekiq::Status::Worker
   require 'net/https'
   require 'uri'
-  include Clockwork
+  require 'clockwork'
   sidekiq_options queue: :broadcast
 
   def expiration
@@ -11,7 +11,6 @@ class BroadcastWorker
   end
 
   def perform(elem)
-    Clockwork.manager = DatabaseEvents::Manager.new
     @clock = ClockWorkEvent.find_by(user_id: elem)
 
     handler do |job|
@@ -31,22 +30,22 @@ class BroadcastWorker
           http.request(req)
         end
       end
+    end
 
-      container = @clock.period_id
-      timer = @clock.send_time.strftime("%H:%M")
+    container = @clock.period_id
+    timer = @clock.send_time.strftime("%H:%M")
 
-      case container
-      when 1
-        every(1.day, '1.day.job', at: timer)
-      when 2
-        every(2.days, '2.days.job', at: timer)
-      when 3
-        every(3.days, '3.days.job', at: timer)
-      when 4
-        every(4.days, '4.days.job', at: timer)
-      when 5
-        every(7.days, '1.week.job', at: timer)
-      end
+    case container
+    when 1
+      every(1.day, '1.day.job', at: timer)
+    when 2
+      every(2.days, '2.days.job', at: timer)
+    when 3
+      every(3.days, '3.days.job', at: timer)
+    when 4
+      every(4.days, '4.days.job', at: timer)
+    when 5
+      every(7.days, '1.week.job', at: timer)
     end
   end
 end
