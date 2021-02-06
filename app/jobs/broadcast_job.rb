6c70e55ac56a9f8c0c82c1043a8f43a26f6edf2b
bird-grid2 <bird2.one.regend@gmail.com>
2021-02-06 11:08:02 +0900

    Update broadcast_job.rb

--- app/jobs/broadcast_job.rb
+++ app/jobs/broadcast_job.rb
@@ -1,10 +1,6 @@
 class BroadcastJob < ApplicationJob
   queue_as :broadcast
 
-  def expiration
-    @expiration ||= 60 * 60 * 24 # 1 day
-  end
-
   def perform(elem)
     @clock = ClockWorkEvent.find_by(user_id: elem)
 
