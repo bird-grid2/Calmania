9aa53a91b4c69b70015c8dc468c6a4e00765a5bd
bird-grid2 <bird2.one.regend@gmail.com>
2021-02-06 15:47:33 +0900

    Update broadcast_job.rb

--- app/jobs/broadcast_job.rb
+++ app/jobs/broadcast_job.rb
@@ -1,10 +1,10 @@
 class BroadcastJob < ApplicationJob
   queue_as :broadcast
-  container = 0
-  timer = 0
 
   def perform(model)
     container = model.period_id
     timer = model.send_time.strftime("%H:%M")
+
+    return container, timer
   end
 end
