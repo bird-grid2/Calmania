9d206ee69c11997b9099af6fc901c72cf9579c6b
bird-grid2 <bird2.one.regend@gmail.com>
2021-02-06 14:56:36 +0900

    Update clock.rb

--- clock.rb
+++ clock.rb
@@ -34,18 +34,18 @@ module Clockwork
     container = model_instance.period_id
     timer = model_instance.send_time.strftime("%H:%M")
 
-    action = case container
-             when 1
-              every(1.day, '1.day.job', at: timer)
-             when 2
-              every(2.days, '2.days.job', at: timer)
-             when 3
-              every(3.days, '3.days.job', at: timer)
-             when 4
-              every(4.days, '4.days.job', at: timer)
-             when 5
-              every(7.days, '1.week.job', at: timer)
-             end
+    action =  case container
+              when 1
+                every(1.day, '1.day.job', at: timer)
+              when 2
+                every(2.days, '2.days.job', at: timer)
+              when 3
+                every(3.days, '3.days.job', at: timer)
+              when 4
+                every(4.days, '4.days.job', at: timer)
+              when 5
+                every(7.days, '1.week.job', at: timer)
+              end
   
     BroadcastJob.perform_later(action)
   end
