71f67625ab7f17613ee766e550486f5926e983a4
bird-grid2 <bird2.one.regend@gmail.com>
2021-02-06 17:21:21 +0900

    modified_clock.rb

--- app/jobs/broadcast_job.rb
+++ app/jobs/broadcast_job.rb
@@ -2,9 +2,21 @@ class BroadcastJob < ApplicationJob
   queue_as :broadcast
 
   def perform(model)
-    container = model.period_id
-    timer = model.send_time.strftime("%H:%M")
+    id = ClockWorkEvent.find_by(user_id: model)
+    container = id.period_id
+    timer = id.send_time.strftime("%H:%M")
 
-    return container, timer
+    case container
+    when 1
+      every(1.day, '1.day.job', at: timer)
+    when 2
+      every(2.days, '2.days.job', at: timer)
+    when 3
+      every(3.days, '3.days.job', at: timer)
+    when 4
+      every(4.days, '4.days.job', at: timer)
+    when 5
+      every(7.days, '1.week.job', at: timer)
+    end
   end
 end
--- clock.rb
+++ clock.rb
@@ -33,19 +33,6 @@ module Clockwork
   sync_database_events model: ClockWorkEvent, every: 1.hour do |model_instance|
     id = model_instance.user_id
     BroadcastJob.perform_later(id)
-
-    case container
-    when 1
-      every(1.day, '1.day.job', at: timer)
-    when 2
-      every(2.days, '2.days.job', at: timer)
-    when 3
-      every(3.days, '3.days.job', at: timer)
-    when 4
-      every(4.days, '4.days.job', at: timer)
-    when 5
-      every(7.days, '1.week.job', at: timer)
-    end
   end
 
   configure do |config|
