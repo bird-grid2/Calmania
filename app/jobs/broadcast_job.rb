e94d1179d1abf20b90abef5aea7882a705927a61
bird-grid2 <bird2.one.regend@gmail.com>
2021-02-07 17:05:05 +0900

    modifed_clock.rb

--- app/jobs/broadcast_job.rb
+++ app/jobs/broadcast_job.rb
@@ -1,7 +1,21 @@
 class BroadcastJob < ApplicationJob
   queue_as :broadcast
+  require 'net/https'
+  require 'uri'
 
   def perform(*args)
-    # clockwork is running
+    uri = URI.parse("https://calmania.work/send")
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    message = 'test'
+
+    http.start do
+      req = Net::HTTP::Post.new(uri.path)
+      req.set_form_data({ body: message })
+      http.request(req)
+    end
   end
 end
--- clock.rb
+++ clock.rb
@@ -1,5 +1,3 @@
-require 'net/https'
-require 'uri'
 require 'clockwork'
 require 'clockwork/database_events'
 require 'active_support/time'
@@ -9,26 +7,12 @@ require File.expand_path('./config/environment', __dir__)
 module Clockwork
   Clockwork.manager = DatabaseEvents::Manager.new
 
-  action = handler do |job|
+  handler do |job|
     case job
     when '1.day.job' || '2.days.job' || '3.days.job' || '4.days.job' || '1.week.job'
-      uri = URI.parse("https://calmania.work/send")
-      http = Net::HTTP.new(uri.host, uri.port)
-
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      message = 'test'
-
-      http.start do
-        req = Net::HTTP::Post.new(uri.path)
-        req.set_form_data({ body: message })
-        http.request(req)
-      end
+      BroadcastJob.perform_later
     end
   end
-
-  BroadcastJob.perform_later(action)
 
   sync_database_events model: ClockWorkEvent, every: 1.hour do |model_instance|
     container = model_instance.period_id
