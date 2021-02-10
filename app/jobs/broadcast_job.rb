998012fcdd64bbe61b427aa67a0274554d36946c
bird-grid2 <bird2.one.regend@gmail.com>
2021-02-11 06:15:04 +0900

    modified_clockwork.rb

--- app/jobs/broadcast_job.rb
+++ app/jobs/broadcast_job.rb
@@ -1,7 +1,23 @@
 class BroadcastJob < ApplicationJob
   queue_as :broadcast
 
-  def perform(*args)
-    # do something
+  def perform(job_name)
+    require 'net/https'
+    require 'uri'
+
+    puts "clockwork run #{job_name}"
+    uri = URI.parse("https://calmania.work/send")
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    message = 'test'
+
+    http.start do
+      req = Net::HTTP::Post.new(uri.path)
+      req.set_form_data({ body: message })
+      http.request(req)
+    end
   end
 end
--- clock.rb
+++ clock.rb
@@ -1,5 +1,3 @@
-require 'net/https'
-require 'uri'
 require 'clockwork'
 require 'clockwork/database_events'
 require 'active_support/time'
@@ -12,19 +10,7 @@ module Clockwork
   handler do |job|
     case job
     when '1.day.job' || '2.days.job' || '3.days.job' || '4.days.job' || '1.week.job'
-      uri = URI.parse("https://calmania.work/send")
-      http = Net::HTTP.new(uri.host, uri.port)
-
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      message = 'test'
-
-      http.start do
-        req = Net::HTTP::Post.new(uri.path)
-        req.set_form_data({ body: message })
-        http.request(req)
-      end
+        BroadcastJob.perform_later(job)
     end
   end
 
@@ -37,6 +23,14 @@ module Clockwork
       exit
     when 1
       every(1.day, '1.day.job', at: timer)
+    when 2
+      every(2.days, '2.days.job', at: timer)
+    when 3
+      every(3.days, '3.days.job', at: timer)
+    when 4
+      every(4.days, '4.days.job', at: timer)
+    when 5
+      every(1.week, '1.week.job', at: timer)
     end
   end
 
