b17e5249b00cf61cf6add560094c50fc6047324c
bird-grid2 <bird2.one.regend@gmail.com>
2021-04-13 05:51:25 +0900

    Update graph.js

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -2,18 +2,18 @@ $(document).on('turbolinks:load', function(){
   if(document.URL.match(/graph/)){
     var html = `<div class="graph_upper">
                   <div class="left_graph">
-                    <%= image_tag("test.png") %>
+                    <img src="${asset_path('test.png')}">
                   </div>
                   <div class="right_graph">
-                    <%= image_tag("test_1.png") %>
+                    <img src="${assets_path('test_1.png')}">
                   </div>
                 </div>
                 <div class="graph_bottom">
                   <div class="left_graph">
-                    <%= image_tag("test_2.png") %>
+                    <img src="${asset_path('test_2.png')}">
                   </div>
                   <div class="right_graph">
-                    <%= image_tag("test_3.png") %>
+                    <img src="${asset_path('test_3.png')}">
                   </div>
                 </div>`
 
