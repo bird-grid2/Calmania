4d9adcfba70c6fa6eb1d8bb1abc79257e806d0f9
bird-grid2 <bird2.one.regend@gmail.com>
2021-04-14 12:43:29 +0900

    modified_graph_function_22

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,24 +1,22 @@
-$(document).on('turbolinks:load', function(){
-  if(document.URL.match(/graph/)){
-    $(".graph_wrapper").empty();
-    
-    var html = `<div class="graph_upper">
-                  <div class="left_graph">
-                    <img src="/assets/test_${gon.user_id}.png">
-                  </div>
-                  <div class="right_graph">
-                    <img src="/assets/test_${gon.user_id}_1.png">
-                  </div>
-                </div>
-                <div class="graph_bottom">
-                  <div class="left_graph">
-                    <img src="/assets/test_${gon.user_id}_2.png">
-                  </div>
-                  <div class="right_graph">
-                    <img src="/assets/test_${gon.user_id}_3.png">
-                  </div>
-                </div>`
+if(document.URL.match(/graph/)){
+  $(".graph_wrapper").empty();
 
-    $(".graph_wrapper").append(html);
-  };
-});
+  var html = `<div class="graph_upper">
+                <div class="left_graph">
+                  <img src="/assets/test_${gon.user_id}.png">
+                </div>
+                <div class="right_graph">
+                  <img src="/assets/test_${gon.user_id}_1.png">
+                </div>
+              </div>
+              <div class="graph_bottom">
+                <div class="left_graph">
+                  <img src="/assets/test_${gon.user_id}_2.png">
+                </div>
+                <div class="right_graph">
+                  <img src="/assets/test_${gon.user_id}_3.png">
+                </div>
+              </div>`
+
+  $(".graph_wrapper").append(html);
+};
