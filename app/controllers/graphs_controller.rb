1266c904653a2eae9fcd79537280dbb0d08c040a
bird-grid2 <bird2.one.regend@gmail.com>
2021-04-15 06:28:24 +0900

    modified_graph_function

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -5,6 +5,7 @@ class GraphsController < ApplicationController
   require 'numpy'
   before_action :authenticate_user!
   before_action :set_user, only: :index
+  before_action :reset_graph, only: :index
   before_action :set_graph, only: :index
 
   def index; end
@@ -95,7 +96,7 @@ class GraphsController < ApplicationController
       dirpath = "app/assets/images" 
     elsif Rails.env.production?
       dirpath = os.getcwd()
-      dirpath += "/public/assets/"
+      dirpath += "/app/assets/images/"
     end
 
     return unless File.exist?(os.path.join(dirpath, "test_#{@user.id}_1.png"))
