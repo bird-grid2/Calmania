26312582b0bfdab843a2bbb28fc965e059864375
bird-grid2 <bird2.one.regend@gmail.com>
2021-04-17 23:26:29 +0900

    modified_graph_function

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,7 +2,6 @@ class GraphsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: :index
   before_action :set_graph, only: :index
-  before_action :reset_cache, only: :index
 
   def index; end
 
