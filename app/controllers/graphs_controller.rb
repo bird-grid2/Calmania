1c0b149854cb7fa2e0eff56a618a37d69eadd6d3
bird-grid2 <bird2.one.regend@gmail.com>
2021-04-18 20:31:12 +0900

    Update graphs_controller.rb

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,8 +2,7 @@ class GraphsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: :index
   before_action :reset_cache, only: :index
-  before_action :set_graph_left, only: :index
-  before_action :set_graph_right, only: :index
+  before_action :set_graph, only: :index
 
   def index
     expires_now
@@ -15,54 +14,7 @@ class GraphsController < ApplicationController
     @user = User.find(current_user.id)
   end
 
-  def set_graph_left
-    matplotlib = Matplotlib
-    matplotlib.use('Agg')
-    plt = matplotlib::Pyplot
-    np = Numpy
-    os = PyCall.import_module('os')
-    
-    if Rails.env.development?
-      dirpath = "app/assets/images/" 
-    elsif Rails.env.production?
-      dirpath = os.getcwd()
-      dirpath += "/public/assets"
-    end
-
-    result = Log.where(user_id: current_user.id).includes(:user).order(date: 'ASC')
-    date = []
-    weight = []
-    fat = []
-
-    result.each do |w|
-      date << w.date.strftime("%m/%d").to_s
-      weight << w.weight.to_s
-      fat << w.bfp.to_s
-    end
-
-    x = np.array(date)
-    y = np.array(weight)
-    y = y.astype(np.float32)
-
-    plt.title('Height')
-    plt.xlabel('measurement date')
-    plt.ylabel('weight [kg]')
-    plt.plot(x,y)
-    plt.savefig(os.path.join(dirpath, "test_#{@user.id}_1.png"))
-    plt.close()
-
-    y = np.array(fat)
-    y = y.astype(np.float32)
-    
-    plt.title('Body Fat Percentage')
-    plt.xlabel('measurement date')
-    plt.ylabel('Body Fat [%]')
-    plt.plot(x, y)
-    plt.savefig(os.path.join(dirpath, "test_#{@user.id}_3.png"))
-    plt.close()
-  end
-
-  def set_graph_right
+  def set_graph
     matplotlib = Matplotlib
     matplotlib.use('Agg')
     plt = matplotlib::Pyplot
@@ -79,35 +31,59 @@ class GraphsController < ApplicationController
     result = Log.where(user_id: current_user.id).includes(:user).order(date: 'ASC')
     height = User.find(current_user.id).height
     date = []
+    weight = []
     total = []
+    fat = []
     bmi = []
 
     result.each do |w|
       date << w.date.strftime("%m/%d").to_s
+      weight << w.weight.to_s
       total << w.total_cal.to_s
+      fat << w.bfp.to_s
       bmi << (w.weight / ((height / 100) ** 2)).to_s
     end
 
-    x = np.array(date)
-    y = np.array(total)
-    y = y.astype(np.float32)
+      x = np.array(date)
+      y = np.array(weight)
+      y = y.astype(np.float32)
 
-    plt.title('Total Calory')
-    plt.xlabel('measurement date')
-    plt.ylabel('Calory [kCal]')
-    plt.plot(x, y)
-    plt.savefig(os.path.join(dirpath, "test_#{@user.id}_2.png"))
-    plt.close()
+      plt.title('Height')
+      plt.xlabel('measurement date')
+      plt.ylabel('weight [kg]')
+      plt.plot(x,y)
+      plt.savefig(os.path.join(dirpath, "test_#{@user.id}_1.png"))
+      plt.close()
 
-    y = np.array(bmi)
-    y = y.astype(np.float32)
+      y = np.array(total)
+      y = y.astype(np.float32)
 
-    plt.title('Body Mass Index')
-    plt.xlabel('measurement date')
-    plt.ylabel('BMI [-]')
-    plt.plot(x, y)
-    plt.savefig(os.path.join(dirpath, "test_#{@user.id}_4.png"))
-    plt.close()
+      plt.title('Total Calory')
+      plt.xlabel('measurement date')
+      plt.ylabel('Calory [kCal]')
+      plt.plot(x, y)
+      plt.savefig(os.path.join(dirpath, "test_#{@user.id}_2.png"))
+      plt.close()
+
+      y = np.array(fat)
+      y = y.astype(np.float32)
+      
+      plt.title('Body Fat Percentage')
+      plt.xlabel('measurement date')
+      plt.ylabel('Body Fat [%]')
+      plt.plot(x, y)
+      plt.savefig(os.path.join(dirpath, "test_#{@user.id}_3.png"))
+      plt.close()
+
+      y = np.array(bmi)
+      y = y.astype(np.float32)
+
+      plt.title('Body Mass Index')
+      plt.xlabel('measurement date')
+      plt.ylabel('BMI [-]')
+      plt.plot(x, y)
+      plt.savefig(os.path.join(dirpath, "test_#{@user.id}_4.png"))
+      plt.close()
   end
 
   def reset_cache
