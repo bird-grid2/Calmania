84432cea4d37291e9f002260a406150a7b6c11f8
bird-grid2 <bird2.one.regend@gmail.com>
2021-05-29 15:14:52 +0900

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,12 +4,12 @@ require 'sidekiq-status'
 if Rails.env.production?
   Sidekiq.configure_server do |config|
     config.redis = { url: ENV.fetch('REDIS_URL', 'redis://redis-calmania.lqvkik.0001.apne1.cache.amazonaws.com:6379'), namespace: 'sidekiq' }
-    Sidekiq::Status.configure_server_middleware config, expiration: 1.week
-    Sidekiq::Status.configure_client_middleware config, expiration: 1.week
+    Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
+    Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
   end
 
   Sidekiq.configure_client do |config|
     config.redis = { url: ENV.fetch('REDIS_URL', 'redis://redis-calmania.lqvkik.0001.apne1.cache.amazonaws.com:6379'), namespace: 'sidekiq' }
-    Sidekiq::Status.configure_client_middleware config, expiration: 1.week
+    Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
   end
 end
