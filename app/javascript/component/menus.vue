3c1808c2831a007e15d096c875fb783e47e42679
bird-grid2 <bird2.one.regend@gmail.com>
2021-06-02 22:12:01 +0900

    create_menus.vue

---
+++ app/javascript/component/menus.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="wrapper">
+    <div class='side-management'>
+        <router-view to='/management'>
+          <i class='fas fa-home'>::before</i>
+          <h6 class='pc'>Home</h6>
+          <h6 class='sp'>Home</h6>
+        </router-view>
+        <span></span>
+        <router-link to='new_menu_path'>
+          <i class='fas fa-plus-circle'>::before</i>
+          <h6 class='pc'>Create<br>menus</h6>
+          <h6 class='sp'>Create menus</h6>
+        </router-link>
+        <span></span>
+        <router-link to='destroy_user_session_path' method: :delete>
+          <i class='fas fa-sign-out-alt'>::before</i>
+          <h6 class='pc'>Sign<br>out</h6>
+          <h6 class='sp'>Sign out</h6>
+        </router-link>
+    </div>
+    <div class='menus_wrapper'>
+      <div class='upper-info'>
+        <table>
+          <tbody>
+            <tr>
+              <th class='table-icon'></th>
+              <th class='table-title'>Menu</th>
+              <th class='table-title'>総重量 [g]</th>
+              <th class='table-title'>タンパク質 [kCal]</th>
+              <th class='table-title'>脂質 [kCal]</th>
+              <th class='table-title'>炭水化物 [kCal]</th>
+            </tr>
+            <% @menus.each do |menu| %>
+              <tr>
+                <td class='table-icon'>
+                  = link_to "menus/#{menu.id}/edit" do
+                    <i class='fas fa-edit'>::before</i>
+                  <span></span>
+                  = link_to "menus/#{menu.id}", data: { confirm: "本当に削除しますか?" }, method: :delete do
+                    <i class='fas fa-trash-alt'>::before</i>
+                </td>
+                <td class='table-item'>
+                  <%= menu.material %>
+                </td>
+                <td class='table-item'>
+                  <%= menu.masses.sum{ |num| num.to_i } %>
+                </td>
+                <td class='table-item'>
+                  <%= menu.total_protain %>
+                </td>
+                <td class='table-item'>
+                  <%= menu.total_fat %>
+                </td>
+                <td class='table-item'>
+                  <%= menu.total_carbohydrate %>
+                </td>
+              </tr>
+            <% end %>
+          </tbody>
+        </table>
+      </div>
+      <div class='bottom-info'>
+        <%= form_with url: search_menus_path, method: :get, class: 'search-box', local: true do |f| %>
+          <%= f.text_field :keyword, placeholder: 'メニューを入力して下さい', class: 'search-input' %>
+      </div>
+    </div>
+  </div>
+</template>
