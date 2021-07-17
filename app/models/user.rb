c2a4be0bacc3d26880a4b0a13ae4956e5751656e
bird-grid2 <bird2.one.regend@gmail.com>
2021-07-17 09:40:32 +0900

    modified_rspec_indicated_thing

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_one :clock_work_event, dependent: :destroy
   accepts_nested_attributes_for :clock_work_event, allow_destroy: true
 
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
   validates :height, :ideal_protain_rate, :ideal_fat_rate, :ideal_carbohydrate_rate, :target_cal, numericality: { allow_nil: true }
 end
