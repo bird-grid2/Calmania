1ea07386cde1776396ff8f6ab5814c8af3e08662
bird-grid2 <bird2.one.regend@gmail.com>
2021-07-24 13:56:40 +0900

    Update sessions_controller.rb

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -4,17 +4,14 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   before_action :authenticate_user!, except: [:new, :create]
   before_action :configure_sign_in_params, only: [:create]
 
+  # GET /resource/sign_in
   def new
-    @user = User.new(sign_in_params)
-    render json: @user
+    super
   end
-    
-  def create
-    @user = User&.authenticate(params[:user][:email], params[:user][:nickname], params[:user][:password])
 
-    if @user.present?
-      render json: @user
-    end
+  # POST /resource/sign_in
+  def create
+    super
   end
 
   protected
@@ -34,24 +31,7 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   end
 end
 
-# GET /resource/sign_in
-# def new
-#   super
-# end
-
-# POST /resource/sign_in
-# def create
-#   super
-# end
-
 # DELETE /resource/sign_out
 # def destroy
 #   super
-# end
-
-# protected
-
-# If you have extra params to permit, append them to the sanitizer.
-# def configure_sign_in_params
-#   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
 # end
