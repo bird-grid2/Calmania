dbfc3176a5aeba48b731fbfcb31f4a926a31f9f1
bird-grid2 <bird2.one.regend@gmail.com>
2023-11-13 22:13:52 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,17 @@ Rails.application.routes.draw do
   mount Sidekiq::Web, at: "/sidekiq"
   post '/callback', to: 'webhook#callback'
   post '/send', to: 'webhook#broadcast'
-  root to: "api/v1/shows#index"
+
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
+
+  resources :users do
+    resources :clock_work_events, except: [:index, :show]
+  end
+  root to: "shows#index"
+  # root to: "api/v1/shows#index"
 
   namespace 'api', defaults: { format: :json } do
     namespace 'v1' do
