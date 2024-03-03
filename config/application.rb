5b701b3b64a15aee81aedeb8149e1d52cb3c62d7
bird-grid2 <bird2.one.regend@gmail.com>
2024-03-04 00:11:37 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,13 @@ module Calmania
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.i18n.default_locale = :ja
+    config.active_job.queue_adapter = :sidekiq
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
     config.autoload_once_paths << "#{root}/app/serializers"
     # Configuration for the application, engines, and railties goes here.
     #
@@ -28,5 +30,12 @@ module Calmania
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+    config.autoload_paths += ['public/assets']
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
   end
 end
