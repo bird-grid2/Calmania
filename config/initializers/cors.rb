f49b39747470fdd6015e831692db8df35b484768
bird-grid2 <bird2.one.regend@gmail.com>
2024-05-25 22:50:07 +0900

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials.dig(Rails.env.to_sym, :cors_origins)
+    #origins Rails.application.credentials.dig(Rails.env.to_sym, :cors_origins)
+    origins = Rails.application.credentials.dig(Rails.env.to_sym, :cors_origins)
+    puts "CORS origin: #{origins.inspect}"  # デバッグ出力
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
