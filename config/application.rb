04b04fad81b09d49af0e1fef2ba42b426d160f01
bird-grid2 <bird2.one.regend@gmail.com>
2024-05-29 21:52:49 +0900

    modified application.rb

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Calmania
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    # config.autoload_paths += ['public/assets']
+    config.autoload_paths += ['public/assets']
     config.middleware.use ActionDispatch::Flash
     config.middleware.use ActionDispatch::Cookies
   end
