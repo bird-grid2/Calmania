6a9137ebda47786e029be6dc72a2d4367b005aef
bird-grid2 <bird2.one.regend@gmail.com>
2024-05-29 11:49:36 +0900

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials.dig(Rails.env.to_sym, :cors_origins)
+    origins ENV["API_PRODUCTION_URL"] || ENV["API_BASE_URL"]
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
